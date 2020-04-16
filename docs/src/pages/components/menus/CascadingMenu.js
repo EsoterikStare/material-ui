ff9b71e37a6f1ce8dcf2a3149cc09d4aed8dd6e8
Erik Burton <eburton@alteryx.com>
2020-04-15 18:14:01 -0600

    Fix broken demo

--- docs/src/pages/components/menus/CascadingMenu.js
+++ docs/src/pages/components/menus/CascadingMenu.js
@@ -3,10 +3,19 @@ import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import Switch from '@material-ui/core/Switch';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles((theme) => ({
+  switchWrapper: {
+    marginLeft: theme.spacing(2),
+  },
+}));
 
 export default function CascadingMenu() {
   const [anchorEl, setAnchorEl] = useState(null);
   const [darkMode, setDarkMode] = useState(false);
+
+  const classes = useStyles();
 
   const handleButtonClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -25,12 +34,21 @@ export default function CascadingMenu() {
       <Button aria-controls="cascading-menu" aria-haspopup="true" onClick={handleButtonClick}>
         Open Menu
       </Button>
-      <Menu id="cascading-menu" anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+      <Menu
+        id="cascading-menu"
+        anchorEl={anchorEl}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
         <MenuItem
           subMenu={
             <Menu>
               <MenuItem onClick={swapDarkMode}>
-                Dark Mode <Switch style={{ marginLeft: '8px' }} size="small" checked={darkMode} />
+                Dark Mode
+                <div className={classes.switchWrapper}>
+                  <Switch size="small" checked={darkMode} />
+                </div>
               </MenuItem>
               <MenuItem
                 onClick={handleClose}
