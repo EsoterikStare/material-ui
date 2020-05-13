9eb1c4a3ad1330b34df1c2a17c7aa710eea14bd5
Erik Burton <eburton@alteryx.com>
2020-05-13 13:53:14 -0600

    Revert "REVERT: Adding reproduction setup to Cascading Menu demo"
    This reverts commit 48411403400464884494778d52582d4aea41dda2.

--- docs/src/pages/components/menus/CascadingMenu.js
+++ docs/src/pages/components/menus/CascadingMenu.js
@@ -14,96 +14,14 @@ export default function CascadingMenu() {
   const handleButtonClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
-  
+
   const handleClose = () => {
     setAnchorEl(null);
   };
-  
+
   const swapDarkMode = () => {
     setDarkMode(!darkMode);
   };
-  
-  const candies = [
-    'Lifesavers',
-    'Hersheys Kiss',
-    'Skittles',
-    'Twizzlers',
-    'Ferrero Rocher',
-    'Reeses Pieces',
-    'Dum Dums Pop',
-    'Starburst',
-    'Swedish Fish',
-    'Airheads',
-    'Kitkat',
-    'Almond Joy',
-    'Twix',
-    '3 Musketeers',
-    'Milky Way',
-    'Tootsie Roll',
-    'Tootsie Pop',
-    'Werthers',
-    'Andes Mint',
-    'Sour Patch Kids',
-    'Milk Duds',
-    'Sweet Tarts',
-    'Nerds',
-    'Laffy Taffy',
-    'Gobstopper',
-    'Mounds',
-    'Snickers',
-    'York Peppermint Pattie',
-    'Heath Bar',
-    'Jolly Rancher',
-    'Blow Pop',
-    '100 Grand',
-    'Crunch',
-    'Butterfinger',
-    'Baby Ruth',
-    'Dove Bar',
-    'Lemonhead',
-    'Warheads',
-    '5th Avenue',
-    'Bar None',
-    'Clark Bar',
-    'Krackel',
-    'Bueno',
-    'Lindt Chocolate Bar',
-    'Lindt Lindor Truffles',
-    'Mars Bar',
-    'Mr. Goodbar',
-    'Milka',
-    'Pay Day',
-    'Take 5',
-    'Toblerone',
-    'U-No Bar',
-    'Wonka Bar',
-    'Whatchamacallit',
-    'Runts',
-    'Bubble Tape',
-    'Candy Buttons',
-    'Candy Cigarettes',
-    'Candy Corn',
-    'Dots',
-    'Fun Dip',
-    'Junior Mints',
-    'Peeps',
-    'Pop Rocks',
-    'Pixie Stix',
-    'Pez',
-    'Raisinets',
-    'Razzles',
-    'Smarties',
-    'Whoppers',
-    'Topic',
-    'Hot Tamales',
-    'Life Savers Gummies',
-    'Cookie Dough Bites',
-    'Spree',
-    'Mentos',
-    'Tic Tac',
-    'Sugar Babies',
-    'Haribo Starmix'
-  ].map(candy => <MenuItem onClick={handleClose}>{candy}</MenuItem>)
 
   return (
     <div>
@@ -129,7 +47,9 @@ export default function CascadingMenu() {
               <MenuItem
                 subMenu={
                   <Menu>
-                    {candies}
+                    <MenuItem onClick={handleClose}>75%</MenuItem>
+                    <MenuItem onClick={handleClose}>100%</MenuItem>
+                    <MenuItem onClick={handleClose}>125%</MenuItem>
                   </Menu>
                 }
               >
