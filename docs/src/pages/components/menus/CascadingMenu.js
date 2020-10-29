4f3144e9817834dc60a6f1961d9a88eefdc3184e
Erik Burton <eburton@alteryx.com>
2020-10-28 20:05:38 -0600

    Remove disused imports from CascadingMenu demo

--- docs/src/pages/components/menus/CascadingMenu.js
+++ docs/src/pages/components/menus/CascadingMenu.js
@@ -3,8 +3,6 @@ import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import SubMenu from '@material-ui/core/SubMenu';
-import Switch from '@material-ui/core/Switch';
-import Box from '@material-ui/core/Box';
 import useTheme from '@material-ui/styles/useTheme';
 
 export default function CascadingMenu() {
