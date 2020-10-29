9f9616a73ce017c2387df24786db2d39d68109c5
Erik Burton <eburton@alteryx.com>
2020-10-28 20:08:11 -0600

    Update api of Menu, MenuItem, and SubMenu

--- packages/material-ui/src/SubMenu/SubMenu.d.ts
+++ packages/material-ui/src/SubMenu/SubMenu.d.ts
@@ -1,5 +1,10 @@
 import { MenuProps } from '@material-ui/core/Menu';
 
 export type SubMenuProps = Omit<MenuProps, 'open'>;
-
+/**
+ *
+ * API:
+ *
+ * - [SubMenu API](https://material-ui.com/api/sub-menu/)
+ */
 export default function SubMenu(props: SubMenuProps): JSX.Element;
